5791b314c1f3a113789e312bbcbcb00f70e05393
BonggilB <95198463+BonggilB@users.noreply.github.com>
2021-12-15 18:13:08 +0900

    Add files via upload

--- js/dark-light.js
+++ js/dark-light.js
@@ -1,30 +1,31 @@
 function darkMode(){
-    let body = document.querySelector('body');
-    let mainTitle = document.querySelector('h1');
-    let nav = document.querySelector('ol');
     let linkText = document.querySelectorAll('a');
-    linkText[0].style.color='lightblue';
-    linkText[1].style.color='lightblue';
-    linkText[2].style.color='lightblue';
-    body.style.backgroundColor='black';
-    mainTitle.style.color='white';
-    document.querySelector('div').style.color='white';
-    nav.style.color='white';
-    mainTitle.style.borderBottom = '5px solid white';
+
+    linkText[0].style.color = 'lightblue';
+    linkText[1].style.color = 'lightblue';
+    linkText[2].style.color = 'lightblue';
+
+    document.querySelector('body').style.backgroundColor = 'black';
+    document.querySelector('h1').style.color = 'white';
+    document.querySelector('h1').style.borderBottom = '5px solid white';
+    document.querySelector('ol').style.color = 'white';
+    document.querySelector('h2').style.color = 'white';
+    document.querySelector('p').style.color = 'white';
     document.querySelector('div').style.borderLeft = '5px solid white';
 }
+
 function lightMode(){
-    let body = document.querySelector('body');
-    let mainTitle = document.querySelector('h1');
-    let nav = document.querySelector('ol');
     let linkText = document.querySelectorAll('a');
-    linkText[0].style.color='red';
-    linkText[1].style.color='red';
-    linkText[2].style.color='red';
-    body.style.backgroundColor='white';
-    mainTitle.style.color='black';
-    document.querySelector('div').style.color='black';
-    nav.style.color='black';
-    mainTitle.style.borderBottom = '5px solid black';
+
+    linkText[0].style.color = 'red';
+    linkText[1].style.color = 'red';
+    linkText[2].style.color = 'red';
+
+    document.querySelector('body').style.backgroundColor = 'white';
+    document.querySelector('h1').style.color = 'black';
+    document.querySelector('h1').style.borderBottom = '5px solid black';
+    document.querySelector('ol').style.color = 'black';
+    document.querySelector('h2').style.color = 'black';
+    document.querySelector('p').style.color = 'black';
     document.querySelector('div').style.borderLeft = '5px solid black';
 }
